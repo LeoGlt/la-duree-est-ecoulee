0bfe5c3494360d910ebacdb4931e4afaea456883
LeoGlt <leogillette44@gmail.com>
2024-04-01 13:32:20 +0200

    remove team view (#23)
    Co-authored-by: Léo Gillette <leo@Leos-MacBook-Pro.(none)>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,6 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <h1>La durée est écoulée</h1>
-  <RouterLink class="action" to="/equipes">Jouer</RouterLink>
+  <h2>Faites 2 équipes</h2>
+  <RouterLink class="action" to="/cest-parti-pour-la-manche-1">Jouer</RouterLink>
 </template>
