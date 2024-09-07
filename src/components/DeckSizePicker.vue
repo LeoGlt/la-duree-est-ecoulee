11459b6c9543efddaf1ddecd0793715657d04c29
LeoGlt <leogillette44@gmail.com>
2024-09-07 18:46:03 +0200

    Deck size picker (#62)
    Co-authored-by: Léo Gillette <leo@Leos-MacBook-Pro.(none)>

---
+++ src/components/DeckSizePicker.vue
@@ -0,0 +1,24 @@
+<script setup>
+const emit = defineEmits(['changeDeckSize'])
+
+const defaultDeckSize = +import.meta.env.VITE_DEFAULT_DECK_SIZE
+
+emit('changeDeckSize', defaultDeckSize)
+</script>
+
+<template>
+  <h2>Combien veux-tu de cartes ?</h2>
+  <select @change="(event) => emit('changeDeckSize', event.target.value)">
+    <option v-for="i in 10" :key="i" :value="5 * i" :selected="5 * i === defaultDeckSize">
+      {{ 5 * i }}
+    </option>
+  </select>
+</template>
+
+<style scoped>
+select {
+  font-size: 2rem;
+  text-align: center;
+  min-width: 30%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,15 @@
 import { RouterLink } from 'vue-router'
 import { initDeck } from '@/components/deck'
 import storageInterface from '@/storage-interface'
+import DeckSizePicker from '../components/DeckSizePicker.vue'
+
+let deckSize
 
 const initGame = () => {
   storageInterface.cardsFound1 = undefined
   storageInterface.cardsFound2 = undefined
   storageInterface.cardsFound3 = undefined
-  initDeck(import.meta.env.VITE_DECK_SIZE)
+  initDeck(deckSize)
   storageInterface.nextTeamToPlay = 1
 }
 </script>
@@ -15,5 +18,8 @@ const initGame = () => {
 <template>
   <h1>La durée est écoulée</h1>
   <h2>Faites 2 équipes !</h2>
+
+  <DeckSizePicker @change-deck-size="(value) => (deckSize = value)" />
+
   <RouterLink class="action" to="/cest-parti-pour-la-manche-1" @click="initGame">Jouer</RouterLink>
 </template>
