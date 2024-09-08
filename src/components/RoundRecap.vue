827e91614b1160140f8c1f9abad579e7202095a6
Nicolas Le Floc'h <nicolas.lefloch.info@gmail.com>
2024-09-08 11:58:17 +0200

    Decoration 5 (#67)
    * Decoration 5
    
    * Decoration 5
    
    ---------

--- src/components/RoundRecap.vue
+++ src/components/RoundRecap.vue
@@ -3,6 +3,7 @@ import storageInterface from '@/storage-interface'
 import { computed } from 'vue'
 import { RouterLink } from 'vue-router'
 import RoundHeader from '@/components/RoundHeader.vue'
+import ScoreBar from '@/components/ScoreBar.vue'
 
 const props = defineProps({
   roundNumber: { type: Number, required: true }
@@ -30,28 +31,24 @@ const getWinnerStr = () => {
     return 'Il y a égalité !'
   }
 }
+
+console.log('coucou')
+
+const team1Score = [...Array(props.roundNumber).keys()].map((i) => getNumberCardsFound(i + 1, 1))
+const team2Score = [...Array(props.roundNumber).keys()].map((i) => getNumberCardsFound(i + 1, 2))
+const totalScore =
+  team1Score.reduce((acc, val) => acc + val, 0) + team2Score.reduce((acc, val) => acc + val, 0)
 </script>
 
 <template>
   <round-header :roundNumber="props.roundNumber"></round-header>
   <h1>Scores</h1>
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-        <th>Equipe 1</th>
-        <th>Equipe 2</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="i of [...Array(props.roundNumber).keys()]" :key="i">
-        <th>Manche {{ i + 1 }}</th>
-        <th>{{ getNumberCardsFound(i + 1, 1) }}</th>
-        <th>{{ getNumberCardsFound(i + 1, 2) }}</th>
-      </tr>
-    </tbody>
-  </table>
-
+  <div class="score-bar-list">
+    <h2>Equipe 1</h2>
+    <score-bar color="primary" :score="team1Score" :total="totalScore"></score-bar>
+    <h2>Equipe 2</h2>
+    <score-bar color="secondary" :score="team2Score" :total="totalScore"></score-bar>
+  </div>
   <div v-if="props.roundNumber < 3">
     <RouterLink class="action" :to="'/cest-parti-pour-la-manche-' + nextRoundNumber"
       >Manche {{ nextRoundNumber }}</RouterLink
@@ -62,3 +59,19 @@ const getWinnerStr = () => {
     <RouterLink class="action" to="/">Accueil</RouterLink>
   </template>
 </template>
+
+<style scoped lang="scss">
+@import '@/assets/main';
+.score-bar-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+h2 {
+  text-align: center;
+  color: $secondary-color;
+  margin: 0;
+}
+</style>
