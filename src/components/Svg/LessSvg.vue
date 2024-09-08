78d99478e94f26e425d415a58db4ed9a82bb618f
nicolas_lefloch <nicolas_lefloch@connect-tech.sncf>
2024-09-08 11:44:36 +0200

    Decoration 6

---
+++ src/components/Svg/LessSvg.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.78 5.13">
+    <path class="cls-1" d="M20.33,5.13H0V0h45.78v5.13h-25.43" fill="white"/>
+  </svg>
+</template>
+
+<style scoped lang="scss">
+svg {
+  margin: 0 auto;
+  display: block;
+  width: 100%;
+}
+</style>
